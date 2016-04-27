ab57ee0825f3427265585fb036778339555c8079
Stephen <skarpath@gmail.com>
2016-04-27 16:30:03 -0400

    Updates java programming examples

--- Java/Different Java Examples/src/Main/DifferentJavaExamples.java
+++ Java/Different Java Examples/src/Main/DifferentJavaExamples.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* 
+    This Java program is simple program to practice programming skills with Java.
+*/
+
+
 package Main;
 
 /**
@@ -20,36 +20,56 @@ public class DifferentJavaExamples {
         //Prints Reverse Message from string
         String Message = "My name is Stephen";
         String reverseAt = " ";
-       // print_reverse(Message, reverseAt); 
+        print_reverse(Message, reverseAt, true);  //If Last part is true shows reversed else no
         
-        FizzBuzz();
+        
+        //Show FizzBuzz with ability to adjust numbers
+        System.out.println("\nFizzBuzz Program\n");
+        FizzBuzz(3, 5);
+        
+        
+        System.out.println("\nSend Email Program\n");
+        
     }
     
-    public static void print_reverse(String message, String splitAt)
+    
+    public static void print_reverse(String message, String splitAt, Boolean showReversed)
     {
-        String[] reverse = message.split(splitAt);
+        String[] reverse = message.split(splitAt);        
         
-        for ( int i = (reverse.length-1); i >= 0; i--)
+        if ( showReversed == true)
         {
+            for ( int i = (reverse.length-1); i >= 0; i--)
+            {
+                System.out.println(reverse[i]);
+            }            
+        }
+        else
+        {
+          for ( int i = 0; i < reverse.length; i++)
+          {
             System.out.println(reverse[i]);
+          }
         }
     }
     
-    public static void FizzBuzz()
+    public static void FizzBuzz(int FizzCharIndex, int BuzzCharIndex)
     {
         //For mutiples of 3 print Fizz
         //For mutiples of 5 print Fizz
         for ( int i = 0; i < 100; i++)
-        {
+        {           
             
-            
-            
-            if (i % 3 == 0)
+            if ( i % FizzCharIndex == 0 && i % BuzzCharIndex == 0 )
             {
-                System.out.println("Fizz" + i);
-            } else if(i % 5 == 0)
+                System.out.println("Fizz Buzz -" + i);
+            }
+            else if (i % FizzCharIndex == 0)
             {
-                System.out.println("Buzz" + i);
+                System.out.println("Fizz -" + i);
+            } else if(i % BuzzCharIndex == 0)
+            {
+                System.out.println("Buzz -" + i);
             }
         }
         
